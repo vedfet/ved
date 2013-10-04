28708e7664f6c7a51b1e36be3a2fd4bbc900f73b
vedfet <vedfet@users.noreply.github.com>
2013-10-04 17:14:32 +0530

    yes

--- iid/index.php
+++ iid/index.php
@@ -10,7 +10,7 @@
 * other free or open source software licenses.
 * See COPYRIGHT.php for copyright notices and details.
 */
-
+/* hi this is test */
 // Set flag that this is a parent file
 define( '_JEXEC', 1 );
 
